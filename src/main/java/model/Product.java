330b6584008df5d325c60b0a3b7966f1bdc20a62
oana.ifrim <oana.ifrim10@yahoo.com>
2024-07-26 10:24:37 +0300

    model classes

---
+++ src/main/java/model/Product.java
@@ -0,0 +1,21 @@
+package model;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    String name;
+    @Nullable
+    String description;
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,22 @@
+package model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    String name;
+    String password;
+    String roles;
+}
